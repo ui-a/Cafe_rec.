d48d802385f6189db57731989ffd74c4d5caa663
ui-a <135043176+ui-a@users.noreply.github.com>
2023-09-18 11:52:58 +0000

    [update]修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,18 +5,18 @@ class Public::SearchesController < ApplicationController
     min_price = params[:min_price]
     max_price = params[:max_price]
     if @range == "Coffee"
-      @record = RecordCoffee.all.released.page(params[:page]).per(5)
+      @record = RecordCoffee.all
       if params[:item_name]
         @record = @record.where('item_name LIKE ?', "%#{params[:item_name]}%")
       if params[:shop_name]
         @record = @record.where('shop_name LIKE ?', "%#{params[:shop_name]}%")
       # 価格絞り込み
       if max_price != '' && max_price != nil && min_price != '' && min_price != nil
-            @record = @record.where("price >= #{min_price} and price <= #{max_price}").compact
+            @record = @record.where("price >= #{min_price} and price <= #{max_price}")
         elsif max_price != '' && max_price != nil
-            @record = @record.where("price <= #{max_price}").compact
+            @record = @record.where("price <= #{max_price}")
         elsif min_price != '' && min_price != nil
-            @record = @record.where("price >= #{min_price}").compact
+            @record = @record.where("price >= #{min_price}")
       end
       end
       end
@@ -40,11 +40,11 @@ class Public::SearchesController < ApplicationController
         @record = @record.where('shop_name LIKE ?', "%#{params[:shop_name]}%")
       # 価格絞り込み
       if max_price != '' && max_price != nil && min_price != '' && min_price != nil
-            @record = @record.where("price >= #{min_price} and price <= #{max_price}").compact
+            @record = @record.where("price >= #{min_price} and price <= #{max_price}")
         elsif max_price != '' && max_price != nil
-            @record = @record.where("price <= #{max_price}").compact
+            @record = @record.where("price <= #{max_price}")
         elsif min_price != '' && min_price != nil
-            @record = @record.where("price >= #{min_price}").compact
+            @record = @record.where("price >= #{min_price}")
       end
       end
       end
